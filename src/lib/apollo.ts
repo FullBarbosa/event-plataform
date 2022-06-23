2a8b754fc9c2130285709b46d17b6200cfad099f
FullBarbosa <diibarbosa22@gmail.com>
2022-06-22 23:58:49 -0300

    feat: integration service Event page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,5 +2,5 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   uri: "https://api-sa-east-1.graphcms.com/v2/cl4o8q5ou14m001xrfxwva5e1/master",
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
